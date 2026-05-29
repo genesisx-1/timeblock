1c0613bb07f3b6f554365c69197dabf6b11e09e5
genesisx-1 <northtexaslimo1@gmail.com>
2026-05-29 16:22:12 -0500

    Update app with new changes

--- server.js
+++ server.js
@@ -213,4 +213,4 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'home.htm
 app.get('/app', requireAuth, (req, res) => res.sendFile(path.join(__dirname, 'public', 'app.html')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log(`Block Day running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Mind Mine running on port ${PORT}`));
